de3a940ef9c8ef11045e1b6767d3e33f5ad29a42
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-07-29 23:55:19 +0300

    task01
    add img

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const gallery = document.querySelector(".gallery")
+const galleryIm = galleryMarkup(galleryItems);
+function galleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+         <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+     }
+
+    )
+} 
+
+gallery.insertAdjacentHTML('beforeend', galleryIm);
+
+
 console.log(galleryItems);
