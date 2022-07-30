d15694ead09d5e9812bd8f3d26d6c652cc6fcf3b
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-07-30 13:12:02 +0300

    1
    sss

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -34,9 +34,29 @@ function onGalleryClick(evt) {
 
 instance.show()
   }
-
 }
 
+// const img = document.querySelector('img')
+Image.addEventListener('click', closeModal)
+function closeModal() {
+  const instance = basicLightbox.create(`
+         <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`, {
+    onShow: (instance) => {
+        instance.element().querySelector('a').onclick = instance.close
+    }
+})
+
+instance.show()
+}
 
 
 
